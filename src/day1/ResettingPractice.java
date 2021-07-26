659bf527df4f7a7feaac309e44b9ef48574a5fc1
Malgorzata Altunsayar <mkowalczyk42@gmail.com>
2021-07-26 11:53:32 -0500

    test case is all done

--- src/day1/ResettingPractice.java
+++ src/day1/ResettingPractice.java
@@ -6,6 +6,10 @@ public class ResettingPractice {
         // sun is o the top
         // part 2 is done
         // part 3 is done
+        // part 4 is done
+        // part 5 is done
+        // Mixed/Soft reset remove the commit from history but keep the local changes
+        // did part 4 part 5 and iam done in one commit
 
     }
 }
