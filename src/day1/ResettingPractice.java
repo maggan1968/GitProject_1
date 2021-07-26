1895b3669d5f78178ab9f9c7b5b07a631c973ca9
Malgorzata Altunsayar <mkowalczyk42@gmail.com>
2021-07-26 11:36:37 -0500

    test case 1 is done

---
+++ src/day1/ResettingPractice.java
@@ -0,0 +1,10 @@
+package day1;
+
+public class ResettingPractice {
+    public static void main(String[] args) {
+        //its nice weather today
+        // sun is o the top
+        //
+
+    }
+}
