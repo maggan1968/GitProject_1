d6feedb4f0f899cbfab181b872f2cd95fa012d43
Malgorzata Altunsayar <mkowalczyk42@gmail.com>
2021-07-29 13:42:32 -0500

    done with all list

--- src/day3/ListPractice.java
+++ src/day3/ListPractice.java
@@ -10,11 +10,20 @@ public class ListPractice {
     }
 }
 /*
-### Task
+### Task1
 
 1. Create a new branch called `list`
 2. Create a new class under `day3` package called `ListPractice`
 3. Make few commits under this branch by making few changes
 4. Move your head to master
 5. Merge `list` branch into `master`
+
+Task2
+### Task 2
+1. Create a new branch called `set`
+2. Create a new class under `day3` package called `SetPractice`
+3. Make few commits under this branch by making few changes
+4. Move your head to master
+5. Remove the `--no--ff` option before merging
+6. Merge `list` branch into `master`
  */
