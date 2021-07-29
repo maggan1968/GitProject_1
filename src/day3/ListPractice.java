d04706d749eea5f46f8df5de2b9b901cd90fcffe
Malgorzata Altunsayar <mkowalczyk42@gmail.com>
2021-07-29 13:31:52 -0500

    started new class for list practice

---
+++ src/day3/ListPractice.java
@@ -0,0 +1,13 @@
+package day3;
+
+public class ListPractice {
+}
+/*
+### Task
+
+1. Create a new branch called `list`
+2. Create a new class under `day3` package called `ListPractice`
+3. Make few commits under this branch by making few changes
+4. Move your head to master
+5. Merge `list` branch into `master`
+ */
