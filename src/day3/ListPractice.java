f6ce5412e2cff62932e519d09f22b20727d44b69
Malgorzata Altunsayar <mkowalczyk42@gmail.com>
2021-07-29 13:34:08 -0500

    added Arrays List

--- src/day3/ListPractice.java
+++ src/day3/ListPractice.java
@@ -1,6 +1,12 @@
 package day3;
 
+import java.util.ArrayList;
+
 public class ListPractice {
+    public static void main(String[] args) {
+        ArrayList<String> lst= new ArrayList();
+
+    }
 }
 /*
 ### Task
