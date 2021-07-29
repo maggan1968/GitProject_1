fed0ecee2f7f667f04cd46c479ff20b972a765a4
Malgorzata Altunsayar <mkowalczyk42@gmail.com>
2021-07-29 13:45:27 -0500

    set practice has started

---
+++ src/day3/SetPractice.java
@@ -0,0 +1,16 @@
+package day3;
+
+public class SetPractice {
+    public static void main(String[] args) {
+        // set is  a collection type thst only store unique item
+    }
+
+}
+/*
+1. Create a new branch called `set`
+2. Create a new class under `day3` package called `SetPractice`
+3. Make few commits under this branch by making few changes
+4. Move your head to master
+5. Remove the `--no--ff` option before merging
+6. Merge `list` branch into `master`
+ */
