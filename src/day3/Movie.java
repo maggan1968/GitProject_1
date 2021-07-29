b1e8edc637b542829ace856704319dccdccd2704
Malgorzata Altunsayar <mkowalczyk42@gmail.com>
2021-07-29 15:28:54 -0500

    awesome stuff from the master

--- src/day3/Movie.java
+++ src/day3/Movie.java
@@ -4,4 +4,5 @@ public class Movie {
 // after some friendly discussion
 //they have decided
 //  watched
-// this is my awesome stuff from the master
+// t|his is my awesome stuff from the master
+}
